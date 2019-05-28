ef04705c276f03cbdab3f3aad049afcdcec80b3d
LEO VITOR DE CAMPOS <180976@facens.br>
2019-05-28 08:24:51 -0300

    Item

---
+++ Networking/Assets/Scripts/GameControllers/PlayerItemPickUp.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerItemPickUp : MonoBehaviour
+{
+    PhotonView PV;
+    GameObject pickupImage;
+    Transform playerTarget;
+    bool _lock;
+
+
+    private void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        if (!PV.IsMine)
+            return;
+        playerTarget = GetComponent<AvatarSetup>().myCamera.transform;
+    }
+
+    private void Update()
+    {
+        if (!PV.IsMine)
+            return;
+        if (_lock)
+        {
+            pickupImage.transform.LookAt(playerTarget);
+            pickupImage.transform.Rotate(0, 180, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!PV.IsMine)
+            return;
+        if (other.CompareTag("Item"))
+        {
+            pickupImage = other.GetComponent<Item>().pickupImage;
+            pickupImage.SetActive(true);
+            _lock = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!PV.IsMine)
+            return;
+        if (other.CompareTag("Item"))
+        {
+            pickupImage.SetActive(false);
+            _lock = false;
+        }
+    }
+}
