05a95963a883e21c83b793011f2b57307b8f8d06
LEO VITOR DE CAMPOS <180976@facens.br>
2019-05-28 10:04:50 -0300

    Pick Up Item

--- Networking/Assets/Scripts/GameControllers/Item.cs
+++ Networking/Assets/Scripts/GameControllers/Item.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+using System.IO;
 
 public class Item : MonoBehaviour
 {
     public GameObject pickupImage;
+    public GameObject instantiated;
+    PhotonView PV;
+
+    private void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        SpawnRandomItem();
+
+    }
 
     public void PickUp()
     {
         Destroy(gameObject);
+        Destroy(instantiated);
     }
+
+    void SpawnRandomItem()
+    {
+        instantiated = PhotonNetwork.InstantiateSceneObject(Path.Combine("PhotonPrefabs", "Itens", "Item"), transform.position, transform.rotation);
+    }
+
+
 }
