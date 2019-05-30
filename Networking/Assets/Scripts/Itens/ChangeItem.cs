e3eef226efa128cceec0cfea310f76f0046ebcb4
Renan Marcelo <fokasrenan@hotmail.com>
2019-05-29 22:27:44 -0300

    Novos Materiais

---
+++ Networking/Assets/Scripts/Itens/ChangeItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChangeItem : MonoBehaviour
+{
+    [Header("Objeto:")]
+    public GameObject[] _objetos;
+
+    [Header("Materiais Bronze _EM ORDEM_")]
+    public Material _materiaisBronze;
+
+    [Header("Materiais Prata _EM ORDEM_")]
+    public Material _materiaisPrata;
+
+    [Header("Materiais Ouro _EM ORDEM_")]
+    public Material _materiaisOuro;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
